da8aa1eefd550e4161413c53e1934163e08594c2
kaitlinchoffmann <kaitlinchoffmann@gmail.com>
2026-08-22 14:58:12 -0400

    printing simple statement and commenting

---
+++ module_1_printing_arithmetic/HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+// This creates a single line comment
+
+/*
+ This creates a multi-line comment.
+ Comments are ignored and won't be printed. They are for you and other developers.
+ Get in the habit of commenting what you are doing! It's good practice.
+*/
+
+    }
+}
